73de45cc2df09271fcc3e6e70dc5244b83f32e69
Lauro Rigon <lauro.rigon@odig.net>
2022-04-30 12:07:06 -0300

    add typescript

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  collectCoverage: true,
-  coverageDirectory: "coverage",
-  coverageProvider: "v8",
-}
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+};
