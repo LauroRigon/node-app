ea3acb59813e802514010c58adaca74aedf8f1e3
Lauro Rigon <lauro.rigon@odig.net>
2022-04-30 17:11:40 -0300

    refactor: error handling

--- src/presentation/routers/login-router.spec.ts
+++ src/presentation/routers/login-router.spec.ts
@@ -1,57 +1,88 @@
-class HttpRequest {
-    body: any
+class HttpRequest<BodyType> {
+    body: BodyType
 
-    setBody(body: Object) {
+    constructor(body: BodyType) {
         this.body = body
     }
 }
 
-class HttpResponse {
-    statusCode: number
+enum StatusCode {
+    OK = 100,
+    CREATED = 201,
+    BAD_REQUEST = 400,
+    UNAUTHORIZED = 401,
+    FORBIDDEN = 403,
+    NOT_FOUND = 404,
+}
 
-    constructor(code: number) {
+class HttpResponse<BodyType> {
+    body?: BodyType
+    statusCode: StatusCode
+
+    constructor(code: StatusCode) {
         this.statusCode = code
     }
 
-    setStatusCode(code: number) {
-        this.statusCode = code
+    static badRequest(message?: string) {
+        return new HttpResponse<ResponseBody>(StatusCode.BAD_REQUEST)
+            .setBody({
+                message: message ?? 'Bad request',
+                data: []
+            })
+    }
+
+    static success() {
+        return new HttpResponse<ResponseBody>(StatusCode.OK)
+    }
+
+    setBody(body: BodyType) {
+        this.body = body
+
+        return this
     }
 }
 
+type LoginBody = {
+    email?: string,
+    password?: string,
+}
+
+type ResponseBody = {
+    message: string
+    data: any[]
+}
 
 class LoginRouter {
-    route(request: HttpRequest): HttpResponse {
+    route(request: HttpRequest<LoginBody>): HttpResponse<ResponseBody> {
         const { email, password } = request.body
         if (!email || !password) {
-            return new HttpResponse(400)
+            return HttpResponse.badRequest('Email and password are required')
         }
 
-        return new HttpResponse(200)
+        return HttpResponse.success()
     }
 }
 
 describe('Login router', () => {
     test('Should return 400 if no email is provided', () => {
         const loginRouter = new LoginRouter()
-        const httpRequest = new HttpRequest()
-        httpRequest.setBody({
+        const httpRequest = new HttpRequest<LoginBody>({
             password: '123456',
         })
 
         const httpResponse = loginRouter.route(httpRequest)
-        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.statusCode).toBe(StatusCode.BAD_REQUEST)
     })
 })
 
 describe('Login router', () => {
     test('Should return 400 if no password is provided', () => {
         const loginRouter = new LoginRouter()
-        const httpRequest = new HttpRequest()
-        httpRequest.setBody({
+        const httpRequest = new HttpRequest({
             email: 'test@gmail.com',
         })
 
         const httpResponse = loginRouter.route(httpRequest)
-        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.statusCode).toBe(StatusCode.BAD_REQUEST)
     })
 })
